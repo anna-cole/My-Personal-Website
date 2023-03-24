9a671eadc2f7f14eca81b4f2ce2d875ce2536289
Anna Cole <acrrj123@gmail.com>
2023-03-23 20:24:30 -0500

    changes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+h2.textContent = "Subscribe to receive 10% discount on your first website with us!";
+document.querySelector("h1").appendChild(h2);
+
+h2.style.fontSize = "30px";
+
+const button = document.querySelector('input[type="submit"]');
+button.addEventListener('click', function() {
+    alert('Your email has been sent successfully.');
+});
