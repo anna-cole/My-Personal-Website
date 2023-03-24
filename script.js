29570dba8f496d179a4ddbaa23ea0434976cfea5
Anna Cole <acrrj123@gmail.com>
2023-03-23 20:36:26 -0500

    changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const h2 = document.createElement("h2");
 h2.textContent = "Subscribe to receive 10% discount on your first website with us!";
 document.querySelector("h1").appendChild(h2);
 
-h2.style.fontSize = "30px";
+h2.style.fontSize = "20px";
 
 const button = document.querySelector('input[type="submit"]');
 button.addEventListener('click', function() {
