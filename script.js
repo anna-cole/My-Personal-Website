6d3f7fee7ade9691058a8396a80d6cc5d52dcf33
Anna Raptidou Cole <acrrj123@gmail.com>
2024-06-10 16:18:49 -0500

    added subheading on html and changed title

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const h2 = document.createElement("h2");
-h2.textContent = "Subscribe to receive 10% discount on your first order!";
-document.querySelector("h1").appendChild(h2);
+// const h2 = document.createElement("h2");
+// h2.textContent = "Subscribe to receive 10% discount on your first order!";
+// document.querySelector("h1").appendChild(h2);
 
-h2.style.fontSize = "20px";
+// h2.style.fontSize = "20px";
 
 const button = document.querySelector('input[type="submit"]');
 button.addEventListener('click', function() {
